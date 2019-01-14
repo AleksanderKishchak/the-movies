f5d67770444d01937a869d65719879a92c6aff6a
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-14 18:45:10 +0200

    add action constant

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -6,28 +6,13 @@ export const GET_POPULAR_MOVIES = 'GET_POPULAR_MOVIES';
 export const MOVIES_REQUEST = 'MOVIES_REQUEST';
 
 export function getMoviesByName(name) {
-  /* return dispatch => {
-    dispatch({ type: MOVIES_REQUEST });
-    fetchMoviesByName(name).then(movies => {
-      dispatch({ type: GET_MOVIES, movies });
-    });
-  }; */
   return { type: GET_MOVIES_BY_NAME, name };
 }
 
 export function getMoviesByPopularity() {
-  /* return dispatch => {
-    dispatch({ type: MOVIES_REQUEST });
-    fetchMoviesByPopularity().then(movies => dispatch({ type: GET_MOVIES, movies }));
-  }; */
   return { type: GET_POPULAR_MOVIES };
 }
 
 export function getMoviesByGenre(genreId) {
-  /* return dispatch => {
-    dispatch({ type: MOVIES_REQUEST });
-    fetchMoviesByGenre(genreId).then(movies => dispatch({ type: GET_MOVIES, movies }));
-  }; */
-
   return { type: GET_MOVIES_BY_GENRE, genreId };
 }
--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,30 +1,36 @@
 /* eslint-disable */
+import {
+  GET_POPULAR_MOVIES,
+  GET_MOVIES_BY_GENRE,
+  GET_MOVIES_BY_NAME,
+  MOVIES_REQUEST
+} from '../actions/movies';
 import { takeEvery, put, call } from 'redux-saga/effects';
 import { fetchMoviesByName, fetchMoviesByPopularity, fetchMoviesByGenre } from '../api/apiCalls';
 
 export function* getPopularMovies() {
   console.log('inside saga');
-  yield put({ type: 'MOVIES_REQUEST' });
+  yield put({ type: MOVIES_REQUEST });
   const data = yield call(fetchMoviesByPopularity);
-  yield put({ type: 'GET_MOVIES', data });
+  yield put({ type: GET_POPULAR_MOVIES, data });
 }
 
 export function* getMoviesByName({ name }) {
-  yield put({ type: 'MOVIES_REQUEST' });
+  yield put({ type: MOVIES_REQUEST });
   const data = yield call(fetchMoviesByName(name));
-  yield put({ type: 'GET_MOVIES', data });
+  yield put({ type: GET_MOVIES_BY_NAME, data });
 }
 
 export function* getMoviesByGenre({ genreId }) {
-  yield put({ type: 'MOVIES_REQUEST' });
+  yield put({ type: MOVIES_REQUEST });
   const data = yield call(fetchMoviesByGenre(genreId));
-  yield put({ type: 'GET_MOVIES', data });
+  yield put({ type: GET_MOVIES_BY_GENRE, data });
 }
 
 export function* watchGettingMovies() {
-  yield takeEvery('GET_POPULAR', getPopularMovies);
-  yield takeEvery('GET_BY_NAME', getMoviesByName);
-  yield takeEvery('GET_BY_GENRE', getMoviesByGenre);
+  yield takeEvery(GET_POPULAR_MOVIES, getPopularMovies);
+  yield takeEvery(GET_MOVIES_BY_NAME, getMoviesByName);
+  yield takeEvery(GET_MOVIES_BY_GENRE, getMoviesByGenre);
 }
 
 /* export function* fetchMovies({ payload = {} }) {
