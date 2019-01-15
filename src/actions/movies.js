a69ce86394d200d872749f5719606dbaf3b89664
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-15 13:06:55 +0200

    delete extra imports

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,8 +1,7 @@
-// import { fetchMoviesByName, fetchMoviesByPopularity, fetchMoviesByGenre } from '../api/apiCalls';
-
 export const GET_MOVIES_BY_NAME = 'GET_MOVIES_BY_NAME';
 export const GET_MOVIES_BY_GENRE = 'GET_MOVIES_BY_GENRE';
 export const GET_POPULAR_MOVIES = 'GET_POPULAR_MOVIES';
+export const GET_MOVIES_SUCCESS = 'GET_MOVIES_SUCCESS';
 export const MOVIES_REQUEST = 'MOVIES_REQUEST';
 
 export function getMoviesByName(name) {
