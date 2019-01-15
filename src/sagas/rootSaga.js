c2521beb70021c4ba1e25191f3129f1cf901f0f1
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-15 20:58:31 +0200

    add sorting saga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
-import { watchGettingMovies } from './saga';
+import watchGettingMovies from './moviesSaga';
+import watchSortingAction from './sortingSaga';
 
 export default function* rootSaga() {
-  yield all([watchGettingMovies()]);
+  yield all([watchGettingMovies(), watchSortingAction()]);
 }
