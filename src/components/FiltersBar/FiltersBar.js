8bd4f1aed3c77349dc669d1a4e38050db08dc940
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-16 01:30:20 +0200

    added sortingbar

--- src/components/FiltersBar/FiltersBar.js
+++ src/components/FiltersBar/FiltersBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FormContainer from '../../containers/FormContainer';
 import GenresListContainer from '../../containers/GenreListContainer';
+import SortingBarContainer from '../../containers/SortingBarContainer';
 
 function FiltersBar() {
   return (
     <div className="filters">
       <FormContainer />
+      <SortingBarContainer />
       <GenresListContainer />
     </div>
   );
