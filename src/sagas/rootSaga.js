be550b8d8e8bb5514437a69650b36db2addd11a1
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-16 01:32:25 +0200

    delete unnecessary function

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,8 +1,7 @@
 import { all } from 'redux-saga/effects';
 
 import watchGettingMovies from './moviesSaga';
-import watchSortingAction from './sortingSaga';
 
 export default function* rootSaga() {
-  yield all([watchGettingMovies(), watchSortingAction()]);
+  yield all([watchGettingMovies()]);
 }
