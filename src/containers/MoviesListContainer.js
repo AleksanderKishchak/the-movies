ced9d75e913e92773e3c2334e293c6577d278468
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-24 00:48:52 +0200

    added error prop

--- src/containers/MoviesListContainer.js
+++ src/containers/MoviesListContainer.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList/MoviesList';
 import { getSortedMovies } from '../selectors/sortingSelector';
 
-const mapStateToProps = state => ({
-  movies: getSortedMovies(state),
-  fetching: state.fetching,
-  activeViewType: state.viewType
+const mapStateToProps = ({
+  movies: {
+    movies, fetching, viewType, error, sortingType
+  }
+}) => ({
+  error,
+  fetching,
+  movies: getSortedMovies({ movies, sortingType }),
+  activeViewType: viewType
 });
 
 export default connect(mapStateToProps)(MoviesList);
