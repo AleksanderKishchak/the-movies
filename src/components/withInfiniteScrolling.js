67114756e6a68bd323fe8a7cbb6e8b7b3ccf2be9
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-24 22:44:34 +0200

    hoc for infite scrolling

---
+++ src/components/withInfiniteScrolling.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+
+export default trashold => Component => {
+  class withInfiniteScrolling extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        reachedBottom: false
+      };
+    }
+
+    componentDidMount() {
+      if (this.scroller) {
+        window.addEventListener('scroll', this.onScroll);
+      }
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('scroll', this.onScroll);
+    }
+
+    onScroll = () => {
+      const availDocumentHeight = document.documentElement.clientHeight;
+      const distanceToBottom = this.scroller.getBoundingClientRect().bottom - availDocumentHeight;
+      const { reachedBottom } = this.state;
+
+      if (distanceToBottom < trashold && !reachedBottom) {
+        this.setState({ reachedBottom: true });
+      } else if (distanceToBottom > trashold && reachedBottom) {
+        this.setState({ reachedBottom: false });
+      }
+    };
+
+    getScrollerRef = node => {
+      this.scroller = node;
+    };
+
+    render() {
+      const { reachedBottom } = this.state;
+
+      return (
+        <>
+          <Component {...this.props} reachedBottom={reachedBottom} />
+          <div style={{ height: 1 }} ref={this.getScrollerRef} />
+        </>
+      );
+    }
+  }
+
+  withInfiniteScrolling.displayName = `withInfiniteScrolling(${Component.displayName
+    || Component.name
+    || 'Component'})`;
+
+  return withInfiniteScrolling;
+};
