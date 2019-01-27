de864a9f6976361604510cce62f6e9319a5c3d46
Aleksander Kishchak <sanya69876@gmail.com>
2019-01-27 22:48:57 +0200

    added go up button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 
-import MoviesList from '../../containers/MoviesListContainer';
+import MoviesListPage from '../../pages/MoviesListPage/MoviesListPage';
 import GenresList from '../../containers/GenresListContainer';
 import withMobileDetection from '../withMobileDetection';
 import Sidebar from '../Sidebar/Sidebar';
 import Content from '../Content/Content';
 import Header from '../Header/Header';
-import MoviePage from '../MoviePage/MoviePage';
+import MoviePage from '../../pages/MoviePage/MoviePage';
 import NotFound from '../NotFound/NotFound';
+import GoUp from '../GoUp/GoUp';
 import './App.sass';
 
 class App extends Component {
@@ -36,16 +37,16 @@ class App extends Component {
   render() {
     const { isMobile } = this.props;
     const { isSidebarOpen } = this.state;
-
     return (
       <div className="App">
+        <GoUp />
         <Header isMobile={isMobile} toggleSidebar={this.toggleSidebar} />
         <Sidebar isMobile={isMobile} isOpen={isSidebarOpen} onClose={this.closeSidebar}>
           <GenresList />
         </Sidebar>
         <Content>
           <Switch>
-            <Route exact path="/" component={MoviesList} />
+            <Route exact path="/" component={MoviesListPage} />
             <Route path="/movie/:id" render={props => <MoviePage {...props} />} />
             <Route component={NotFound} />
           </Switch>
